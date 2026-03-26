d454e429decff576f2945ddea2d0425b26db9f9d
Brandon Hopkins <brandon@caltechweb.com>
2026-03-26 16:20:05 -0700

    Revert "PageSpeed: allow q=30 quality in image optimizer config"
    This reverts commit ac99a95099eb374a7ed35cf9a276ec92478caa59.

--- site/next.config.ts
+++ site/next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    qualities: [30, 75],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
