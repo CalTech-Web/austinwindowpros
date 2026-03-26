61b59c88540bda4f62cf57d3601d42ef7b4d2439
Brandon Hopkins <brandon@caltechweb.com>
2026-03-26 16:20:16 -0700

    Revert "PageSpeed: reduce hero background image quality to q=30"
    This reverts commit 1f3c3937c59a052b374c58a8633d922812cd15fd.

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -216,7 +216,7 @@ export default function HomePage() {
             fill
             className="object-cover opacity-25"
             priority
-            quality={30}
+            quality={45}
           />
         </div>
         <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20 text-white">
